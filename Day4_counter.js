d3eaef339b578dfca05d34d61717209ea9e8211c
Vinit1904 <65153072+Vinit1904@users.noreply.github.com>
2020-08-13 09:07:49 +0530

    Day-4 Assingment

---
+++ Day4_counter.js
@@ -0,0 +1,16 @@
+let counter = 1;
+
+function increament() {
+    let counterEle = document.querySelector("#count")
+    counter = counter + 1;
+
+    counterEle.innerHTML = counter;
+}
+
+
+function decreament() {
+    let counterEle = document.querySelector("#count")
+    counter = counter - 1;
+
+    counterEle.innerHTML = counter;
+}
